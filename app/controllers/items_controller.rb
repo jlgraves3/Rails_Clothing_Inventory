47327a4c74f0d2ddf7ff340df783db58680159a7
Julia Graves <jgraves@wesleyan.edu>
2017-09-08 18:14:15 -0400

    create item controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,43 @@
 class ItemsController < ApplicationController
+	before_action :set_item, only: [:show, :edit, :update, :destroy]
+	def index
+		@items = Item.all
+	end
+
+	def show
+		@item = Item.find(params[:id])
+	end
+
+	def new
+		@item = Item.new
+	end
+
+	def create
+		@item = Item.new(item_params)
+		if @item.valid?
+			@item.save
+			redirect_to @item
+		else
+			alert('error')
+	end
+
+	def edit
+	end
+
+	def update
+		if @item.update(item_params)
+			redirect_to @item
+		else
+			alert('error')
+		end
+
+	private
+
+	def item_params
+		params.require(:author).permit(:brand_id,:color_id,:type_id)
+	end
+
+	def set_item
+		@item = Item.find(params[:id])
+	end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
 	belongs_to :brand
 	belongs_to :color
+	belongs_to :type
 end
