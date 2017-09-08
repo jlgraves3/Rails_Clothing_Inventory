437891597d4254f0f5cf2930ed79ce36cf4ef750
Julia Graves <jgraves@wesleyan.edu>
2017-09-08 17:35:15 -0400

    add foreign keys to item migration

--- db/migrate/20170908212446_create_items.rb
+++ db/migrate/20170908212446_create_items.rb
@@ -5,6 +5,8 @@ class CreateItems < ActiveRecord::Migration[5.1]
     	t.string :size
     	t.integer :price
     	t.string :src
+    	t.belongs_to :color, foreign_key: true, index: true
+    	t.belongs_to :brand, foreign_key: true, index: true
       t.timestamps
     end
   end
