b36f796d0d977e885d0737fd85d482191e3c33f4
Julia Graves <jgraves@wesleyan.edu>
2017-09-08 17:26:26 -0400

    edit migration file

--- db/migrate/20170908212446_create_items.rb
+++ db/migrate/20170908212446_create_items.rb
@@ -1,7 +1,13 @@
 class CreateItems < ActiveRecord::Migration[5.1]
   def change
     create_table :items do |t|
-
+    	t.string :name
+    	t.string :brand
+    	t.string :type
+    	t.string :size
+    	t.integer :price
+    	t.string :color
+    	t.sting :src
       t.timestamps
     end
   end
